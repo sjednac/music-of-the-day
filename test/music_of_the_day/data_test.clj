adc0247c5ec50a7f694b31573b1c615b2adac9ae
Szymon Biliński <szymon.bilinski@gmail.com>
2016-07-10 17:55:57 +0200

    Fixed typo.

--- test/music_of_the_day/data_test.clj
+++ test/music_of_the_day/data_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [music-of-the-day.data :refer :all]))
 
-(deftest test-recommendations-has-sample-data
+(deftest test-recommendation-list-has-sample-data
   (let [list recommendations]
     (is (< 0 (count list)))))
