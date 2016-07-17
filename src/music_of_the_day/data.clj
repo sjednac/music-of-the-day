6fb5f9686e537b2d7cac979044834cd1661543ac
Szymon Biliński <szymon.bilinski@gmail.com>
2016-07-17 16:31:06 +0200

    Replaced random recommendations with Spotify recommendations.

--- src/music_of_the_day/data.clj
+++ src/music_of_the_day/data.clj
@@ -4,24 +4,6 @@
 
 (def recommendations (atom []))
 
-(defn random-recommendations
-  "Provides a list of random recommendations"
-  []
-  '(
-      {
-       :id "f1fa6be3-06c8-450b-894d-f36629e10ee3"
-       :artist "Talking Heads"
-       :track "The Lady Don't Mind"
-       :spotify "https://open.spotify.com/track/6xaYStR8hIimvEmKLwv4Gh"
-      }
-      {
-       :id "598173f8-f215-4cda-82c9-03ffa081706e"
-       :artist "Fad Gadget"
-       :track  "Back To Nature"
-       :spotify "https://open.spotify.com/track/4y8EasTsrOzb7FEPgyaKON"
-      }
-  ))
-
 (defn spotify-album-to-recommendation
   "Converts a Spotify album to a recommendation"
   [album]
--- test/music_of_the_day/data_test.clj
+++ test/music_of_the_day/data_test.clj
@@ -1,7 +1,17 @@
 (ns music-of-the-day.data-test
+  (:import  [com.wrapper.spotify.models SimpleAlbum])
   (:require [clojure.test :refer :all]
+            [music-of-the-day.spotify :as spotify]
             [music-of-the-day.data :refer :all]))
 
-(deftest test-recommendation-list-has-sample-data
-  (let [list (random-recommendations)]
-    (is (< 0 (count list)))))
+(def new-releases-fixture [(doto (new SimpleAlbum) (.setId "123") (.setName "test1") (.setUri "http://test1"))
+                           (doto (new SimpleAlbum) (.setId "456") (.setName "test2") (.setUri "http://test2"))
+                           (doto (new SimpleAlbum) (.setId "789") (.setName "test3") (.setUri "http://test3"))])
+
+(def recommendations-fixture [{:id "123" :name "test1" :uri "http://test1"}
+                              {:id "456" :name "test2" :uri "http://test2"}
+                              {:id "789" :name "test3" :uri "http://test3"}])
+
+(deftest test-update-recommendations
+  (with-redefs [spotify/get-new-releases (fn [country] new-releases-fixture)]
+    (is (= recommendations-fixture (update-recommendations) ))))
